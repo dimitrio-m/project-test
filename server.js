a3109d08f02bd0aa693a04f90cfd2bbb991fbef2
Dimitrio Mandamadiotis <dimitriosmandamadiotis@gmail.com>
2017-03-04 11:25:02 -0400

    Agregada la definición del archivo server.js, donde se configuró Express y se concretó el middleware para el manejo de errores.

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const logger = require('morgan');
+
+const route = require('./routes/index');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  secret: process.env.SECRET || 'estoSeSuponeQueEsSecreto',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err);
+});
+
+app.listen(process.env.PORT || 3000);
